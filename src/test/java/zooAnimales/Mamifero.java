90448a269b97a5070601fb2f2bbb4786a00644b1
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-04-27 23:38:20 -0500

    Prueba 1

--- src/test/java/zooAnimales/Mamifero.java
+++ src/test/java/zooAnimales/Mamifero.java
@@ -1,5 +1,82 @@
 package zooAnimales;
 
-public class Mamifero {
+import java.util.ArrayList;
+
+public class Mamifero extends Animal {
+	
+	private static ArrayList<Mamifero> listado = new ArrayList<>();
+	public static int caballos;
+	public static int leones;
+	private boolean pelaje;
+	private int patas;
+	
+	public Mamifero() {
+		 this(null, 0, null, null,false, 0);
+    }
+	
+	public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
+		 super(nombre, edad, habitat, genero);
+		 this.pelaje = pelaje;
+		 this.patas = patas;
+	     listado.add(this);
+	        setTotalMamiferos(1);
+	}
+
+    public int cantidadMamiferos() {
+        return listado.size();
+    }
+    
+    public static Mamifero crearCaballo(String nombre, int edad, String genero) {
+        caballos++;
+
+        boolean pelaje = true;
+        int patas = 4;
+        String habitat = "pradera";
+        
+        Mamifero caballo = new Mamifero(nombre, edad, habitat, genero, pelaje, patas);
+        	
+        listado.add(caballo);
+        return caballo;
+    }
+    
+    public static Mamifero crearLeon(String nombre, int edad, String genero) {
+        leones++;
+
+        boolean pelaje = true;
+        int patas = 4;
+        String habitat = "selva";
+
+        Mamifero leon = new Mamifero(nombre, edad, habitat, genero, pelaje, patas);
+        listado.add(leon);
+        return leon;
+    }
+    
+    public static void setListado(ArrayList<Mamifero> listado) {
+		Mamifero.listado = listado;
+	}
+
+	public int getCaballos() {
+		return caballos;
+	}
+
+	public int getLeones() {
+		return leones;
+	}
+
+	public boolean isPelaje() {
+		return pelaje;
+	}
+
+	public void setPelaje(boolean pelaje) {
+		this.pelaje = pelaje;
+	}
+
+	public int getPatas() {
+		return patas;
+	}
+
+	public void setPatas(int patas) {
+		this.patas = patas;
+	}
 
 }
